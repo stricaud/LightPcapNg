06977234acc4403d70f3bd2ef716abd36f3f504a
Sebastien Tricaud <sebastien@honeynet.org>
2021-09-30 17:07:00 -0700

    Getting the file description

--- src/python/light_pcapng.cpp
+++ src/python/light_pcapng.cpp
@@ -22,6 +22,9 @@ PcapNg::~PcapNg(void) {
 int PcapNg::OpenRead(char *file, int flag)
 {
   _pcapng = light_pcapng_open_read(file, LIGHT_FALSE);
+  if (_pcapng) {
+    _fileinfo = light_pcang_get_file_info(_pcapng);
+  }
   
   return 0;
 }
@@ -31,5 +34,11 @@ PYBIND11_MODULE(pycapng, m) {
     m.doc() = "LightPcapNG Python Bindings";
     py::class_<PcapNg>(m, "PcapNg")
       .def(py::init<>())
-      .def("OpenRead", &PcapNg::OpenRead);
+      .def("OpenRead", &PcapNg::OpenRead)
+      .def("GetFileComment", &PcapNg::GetFileComment)
+      .def("GetOSDesc", &PcapNg::GetOSDesc)
+      .def("GetHardwareDesc", &PcapNg::GetHardwareDesc)
+      .def("GetUserAppDesc", &PcapNg::GetUserAppDesc)
+      .def("GetFileInfoMajorVersion", &PcapNg::GetFileInfoMajorVersion)
+      .def("GetFileInfoMinorVersion", &PcapNg::GetFileInfoMinorVersion);
 }
--- src/python/light_pcapng.hpp
+++ src/python/light_pcapng.hpp
@@ -12,6 +12,12 @@ public:
   int OpenWrite(char *file);
   int OpenAppend(char *file);
   int Close(void);
+  int GetFileInfoMajorVersion() { if (_fileinfo) { return _fileinfo->major_version; } return 0; }
+  int GetFileInfoMinorVersion() { if (_fileinfo) { return _fileinfo->minor_version; } return 0; }
+  char *GetFileComment() { if (_fileinfo) { if (_fileinfo->file_comment) { return _fileinfo->file_comment; } }; return NULL; }
+  char *GetOSDesc() { if (_fileinfo) { if (_fileinfo->os_desc) { return _fileinfo->os_desc; } }; return NULL; }
+  char *GetHardwareDesc() { if (_fileinfo) { if (_fileinfo->hardware_desc) { return _fileinfo->hardware_desc; } }; return NULL; }
+  char *GetUserAppDesc() { if (_fileinfo) { if (_fileinfo->user_app_desc) { return _fileinfo->user_app_desc; } }; return NULL; }
   // close()
 private:
   light_pcapng_t *_pcapng;
@@ -28,7 +34,8 @@ private:
 // class PcapNgFileInfo {
 // public:
   
-  
+// private:
+
 // };
 
 // class PcapNgBlock {
